65096beebff46e5d51a1508a2b9f0e886a15a1ed
Carotorres26 <caritomiranda13@gmail.com>
2025-06-06 09:36:21 -0500

    url

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
+const sequelize = new Sequelize(process.env.MYSQL_URL || "mysql://root:GzcxBKOlILnTuoonzjgCdGlxfqAZDWzT@centerbeam.proxy.rlwy.net:41836/railway", {
   host: '127.0.0.1',
   dialect: 'mysql',
   logging: false
